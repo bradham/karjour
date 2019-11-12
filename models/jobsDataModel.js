eb82674dc4ad1a4e51a96e18f30d3e5654a520e9
bradham <bradham@users.noreply.github.com>
2019-11-12 14:50:07 -0600

    Implemented more data fields

--- models/jobsDataModel.js
+++ models/jobsDataModel.js
@@ -22,6 +22,15 @@ module.exports = function(sequelize, DataTypes) {
     },
     contactEmail: {
       type: DataTypes.STRING
+    },
+    companyName: {
+      type: DataTypes.STRING
+    },
+    culture: {
+      type: DataTypes.STRING
+    },
+    location: {
+      type: DataTypes.STRING
     }
   });
   return Job;
